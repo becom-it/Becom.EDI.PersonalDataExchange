193e97b3e533c56697d8f8bbbd52cbf9066944f1
Michael Prattinger <mprattinger@outlook.com>
2020-11-10 22:26:29 +0100

    adding get employeelisttest

--- Becom.EDI.PersonalDataExchange.Tests/Helpers/ResponseContents.cs
+++ Becom.EDI.PersonalDataExchange.Tests/Helpers/ResponseContents.cs
@@ -11,5 +11,11 @@ namespace Becom.EDI.PersonalDataExchange.Tests.Helpers
             return "<?xml version=\"1.0\" ?><S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\"><S:Body><ns2:getPersonalResponse xmlns:ns2=\"http://WHEDI1/PersonalDataExchange\"><getPersonal><pnazss>1545.0</pnazss><pnbtrm>1</pnbtrm><pneind>30418</pneind><pnfzop>0.0</pnfzop><pnname>Prattinger                    </pnname><pnpern>5555</pnpern><pnurlg>13</pnurlg><pnurlv>0</pnurlv><pnvgsd>5022</pnvgsd><pnvgsf>5022</pnvgsf><pnvnam>Michael             </pnvnam></getPersonal></ns2:getPersonalResponse></S:Body></S:Envelope>";
 
         }
+
+        public static string GetEmployeeListResponse()
+        {
+            return "<?xml version=\"1.0\" ?><S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\"><S:Body><ns2:getPersonalListResponse xmlns:ns2=\"http://WHEDI1/PersonalDataExchange\"><getPersonalList><personalList><pnbtrm>1</pnbtrm><pnname>Auer                          </pnname><pnpern>5120</pnpern><pnvnam>Martin              </pnvnam></personalList><personalList><pnbtrm>1</pnbtrm><pnname>Augustin                      </pnname><pnpern>8768</pnpern><pnvnam>Doris               </pnvnam></personalList><personalList><pnbtrm>1</pnbtrm><pnname>Augustin                      </pnname><pnpern>3176</pnpern><pnvnam>Karin               </pnvnam></personalList><personalList><pnbtrm>1</pnbtrm><pnname>Augustin                      </pnname><pnpern>1425</pnpern><pnvnam>Manfred             </pnvnam></personalList></getPersonalList></ns2:getPersonalListResponse></S:Body></S:Envelope>";
+
+        }
     }
 }
--- Becom.EDI.PersonalDataExchange.Tests/ZeiterfassungsServiceTests.cs
+++ Becom.EDI.PersonalDataExchange.Tests/ZeiterfassungsServiceTests.cs
@@ -37,5 +37,27 @@ namespace Becom.EDI.PersonalDataExchange.Tests
                 req.Content.ReadAsStringAsync().GetAwaiter().GetResult() == RequestContents.GetEmployeeInfoRequestContent()),
                 ItExpr.IsAny<CancellationToken>());
         }
+
+        [Fact]
+        public async Task GetEmployeeListTest()
+        {
+            //Arrange
+            var mocks = MockHelpers.GetMocks(ResponseContents.GetEmployeeListResponse());
+
+            //Act
+            var service = new ZeiterfassungsService(mocks.logger, mocks.mockFactory.Object, mocks.config);
+
+            var info = await service.GetEmployeeList(Model.Enums.CompanyEnum.Austria);
+            info.Should().BeOfType<EmployeeInfo>();
+
+            mocks.mockHttpMessageHandler.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                req.Method == HttpMethod.Post &&
+                req.RequestUri.ToString() == mocks.config.Endpoint &&
+                req.Content.ReadAsStringAsync().GetAwaiter().GetResult() == RequestContents.GetEmployeeListRequestContent()),
+                ItExpr.IsAny<CancellationToken>());
+        }
     }
 }
